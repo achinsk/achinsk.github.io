368ba78472c19439689798182fad92ce6f828be1
achinsk <98710286+achinsk@users.noreply.github.com>
2025-05-27 02:18:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-'use strict';
+//redirect to the Universal Login Page
+document.getElementById('login').addEventListener('click', async () => {
+  await auth0.loginWithRedirect();
+});
 
-function start(x) {
-    let a;
-    let promptMessage = "Угадай число от 1 до 100";
-
-    const isNumber = function (num) {    
-        return !isNaN(parseFloat(num)) && isFinite(num);
-    }
-
-    function engine() {
-        a = prompt(promptMessage);
-
-        if (a == null) {
-            alert("Игра окончена");
-        } else if (!isNumber(a)) {
-            promptMessage = "Введи число!";
-            engine();
-        } else if (x !== parseFloat(a)) {
-            promptMessage = (x > parseFloat(a)) ? "Загаданное число больше, предлагаю ввести новый вариант" : "Загаданное число меньше, предлагаю ввести новый вариант";
-            engine();
-        } else {
-            alert("Поздравляю, Вы угадали!!!");
-        }
-    }
-    engine();
-}
-
-start(55);
+//in your callback route (<MY_CALLBACK_URL>)
+window.addEventListener('load', async () => {
+  const redirectResult = await auth0.handleRedirectCallback();
+  //logged in. you can get the user profile like this:
+  const user = await auth0.getUser();
+  console.log(user);
+});
